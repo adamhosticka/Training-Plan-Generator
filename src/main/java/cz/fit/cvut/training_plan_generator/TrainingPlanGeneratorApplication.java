34ced3cbdf5ee2c38c96c219e8298d8367e68302
Adam Hoštička <hostiada@fit.cvut.cz>
2022-11-09 18:31:45 +0100

    spring_setup: initialized spring repository

---
+++ src/main/java/cz/fit/cvut/training_plan_generator/TrainingPlanGeneratorApplication.java
@@ -0,0 +1,13 @@
+package cz.fit.cvut.training_plan_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingPlanGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingPlanGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/fit/cvut/training_plan_generator/TrainingPlanGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.fit.cvut.training_plan_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingPlanGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
