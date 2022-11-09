5195b94737099f190d0d805a93fbb228c3512817
Adam Hoštička <hostiada@fit.cvut.cz>
2022-11-09 18:40:59 +0100

    entities: exercise entity created

---
+++ src/main/java/cz/fit/cvut/training_plan_generator/domain/Exercise.java
@@ -0,0 +1,56 @@
+package cz.fit.cvut.training_plan_generator.domain;
+
+import java.io.Serializable;
+import java.util.*;
+public class Exercise implements DomainEntity<Long> {
+    private final Long id;
+    private String name;
+    private Category category;
+    private Set<MuscleGroup> muscleGroups = new HashSet<>();
+
+    public Exercise(Long id, String name, Category category, Set<MuscleGroup> muscleGroups) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.muscleGroups = muscleGroups;
+    }
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Set<MuscleGroup> getMuscleGroups() {
+        return muscleGroups;
+    }
+
+    public void addMuscleGroup(MuscleGroup muscleGroups) {
+        this.muscleGroups.add(Objects.requireNonNull(muscleGroups));
+    }
+
+    @Override
+    public String toString() {
+        return "Exercise{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", category=" + category +
+                ", muscleGroups=" + muscleGroups +
+                '}';
+    }
+}
