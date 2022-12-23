01d94adf93a300108f4ec4aa7669e79184060c51
Adam Hoštička <hostiada@fit.cvut.cz>
2022-12-23 13:52:23 +0100

    frontend: added HomeController

--- src/main/java/cz/cvut/fit/training_plan_generator/TrainingPlanGenerator.java
+++ src/main/java/cz/cvut/fit/training_plan_generator/TrainingPlanGenerator.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestApi {
+public class TrainingPlanGenerator {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestApi.class, args);
+		SpringApplication.run(TrainingPlanGenerator.class, args);
 	}
 
 }
