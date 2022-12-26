addd8d9db73f5f8afa91f8dcadb72a31b98b61eb
Adam Hoštička <hostiada@fit.cvut.cz>
2022-12-26 17:36:23 +0100

    frontend: react app

---
+++ src/main/js/src/app.js
@@ -0,0 +1,17 @@
+import {Component} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from "react-bootstrap/Container";
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Ahoj</h1>
+                <Container>
+                </Container>
+            </div>
+        )
+    }
+}
+
+export default App;
