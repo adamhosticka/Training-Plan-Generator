6423cbdfffc5a25733c23270ecacacf279baa6f1
Adam Hoštička <hostiada@fit.cvut.cz>
2022-12-27 20:20:15 +0100

    rest_config: created config to show ids for musclegroup entity

---
+++ src/main/java/cz/cvut/fit/training_plan_generator/dao/RestConfig.java
@@ -0,0 +1,16 @@
+package cz.cvut.fit.training_plan_generator.dao;
+
+import cz.cvut.fit.training_plan_generator.domain.MuscleGroup;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Component
+public class RestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.exposeIdsFor(MuscleGroup.class);
+    }
+}
