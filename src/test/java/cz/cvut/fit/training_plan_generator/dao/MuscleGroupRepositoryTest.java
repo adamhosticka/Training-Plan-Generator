b595af739eb0d14c4063677794c3019b7e38095e
Adam Hoštička <hostiada@fit.cvut.cz>
2022-12-30 16:02:22 +0100

    tests: muscleGroupRepositoryTest

--- src/test/java/cz/cvut/fit/training_plan_generator/dao/MuscleGroupRepositoryTest.java
+++ src/test/java/cz/cvut/fit/training_plan_generator/dao/MuscleGroupRepositoryTest.java
@@ -6,9 +6,11 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,14 +26,30 @@ public class MuscleGroupRepositoryTest {
     }
 
     @Test
-    void addMuscleGroup() {
+    void muscleGroupSave() {
         MuscleGroup tongue = new MuscleGroup("Tongue", 99);
         this.muscleGroupRepository.save(tongue);
         assertEquals(1, muscleGroupRepository.count());
     }
 
     @Test
-    void testMGRepo() {
-        assertEquals(0, muscleGroupRepository.count());
+    void muscleGroupSaveAllFindAll() {
+        MuscleGroup tongue = new MuscleGroup("Tongue", 99);
+        MuscleGroup calf = new MuscleGroup("Calf", 0);
+        this.muscleGroupRepository.saveAll(Arrays.asList(tongue, calf));
+        assertEquals(Arrays.asList(tongue, calf), muscleGroupRepository.findAll());
+    }
+
+    @Test
+    void muscleGroupExistsByIdFindById() {
+        MuscleGroup tongue = this.muscleGroupRepository.save(new MuscleGroup("Tongue", 99));
+        MuscleGroup calf = this.muscleGroupRepository.save(new MuscleGroup("Calf", 0));
+
+        assertTrue(this.muscleGroupRepository.existsById(tongue.getId()));
+        assertTrue(this.muscleGroupRepository.existsById(calf.getId()));
+        assertFalse(this.muscleGroupRepository.existsById(tongue.getId() + calf.getId()));
+        assertEquals(Optional.of(tongue), this.muscleGroupRepository.findById(tongue.getId()));
+        assertEquals(Optional.of(calf), this.muscleGroupRepository.findById(calf.getId()));
+        assertEquals(Optional.empty(), this.muscleGroupRepository.findById(999L));
     }
 }
