c8cd79d15b8d96a6994610bf4300ec9ae096a9aa
Adam Hoštička <hostiada@fit.cvut.cz>
2023-01-01 21:20:49 +0100

    security: denied all delete requests to /muscleGroups/*

--- src/main/java/cz/cvut/fit/training_plan_generator/TrainingPlanGenerator.java
+++ src/main/java/cz/cvut/fit/training_plan_generator/TrainingPlanGenerator.java
@@ -2,8 +2,13 @@ package cz.cvut.fit.training_plan_generator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 @ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = DatabaseLoader.class)) // disable loading db
@@ -13,4 +18,16 @@ public class TrainingPlanGenerator {
 		SpringApplication.run(TrainingPlanGenerator.class, args);
 	}
 
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and()
+				.cors().disable()
+				.csrf().disable();
+		httpSecurity.authorizeRequests()
+				.antMatchers(HttpMethod.DELETE, "/muscleGroups/*")
+				.denyAll();
+		return httpSecurity.build();
+	}
 }
